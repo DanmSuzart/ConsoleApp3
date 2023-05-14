18a2e029bd449e87556f3e4a70f988c5f409a680
DanmSuzart <eudanielsuzart@gmail.com>
2023-05-14 12:32:55 -0300

    ListaDeFrutas

---
+++ ConsoleApp3/Exercico1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Exercico1
+    {
+        public static void Executar()
+        {
+            List<String> listFruit = new List<String>();
+
+            string sair = string.Empty;
+
+            do
+            {
+                Console.WriteLine("Informe uma fruta: ");
+                string fruitName = Console.ReadLine();
+
+                listFruit.Add(fruitName);
+
+                Console.WriteLine("Deseja continuar? S/N");
+                sair = Console.ReadLine().ToUpper();
+
+
+            } while (sair != "N");
+
+
+            foreach (string fruit in listFruit)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+    }
+}
